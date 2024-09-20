36531fa62a591692bdd608de042e09f4f2336333
Maxim <moxalehin@gmail.com>
2024-09-20 14:31:55 +0300

    Add plugin's files

---
+++ Source/ColorRamp/ColorRamp.Build.cs
@@ -0,0 +1,47 @@
+using UnrealBuildTool;
+
+public class ColorRamp : ModuleRules
+{
+	public ColorRamp(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+                
+			}
+			);
+	}
+}
---
+++ Source/ColorRamp/Private/ColorRamp.cpp
@@ -0,0 +1,17 @@
+#include "ColorRamp.h"
+
+#define LOCTEXT_NAMESPACE "FColorRampModule"
+
+void FColorRampModule::StartupModule()
+{
+	
+}
+
+void FColorRampModule::ShutdownModule()
+{
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FColorRampModule, ColorRamp)
